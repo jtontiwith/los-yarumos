5b8088f86bb0460fce57ff760786349e294d56ef
jtontiwith <jtonti@gmail.com>
2022-04-09 14:51:49 -0500

    hmm

--- components/ImgTest.vue
+++ components/ImgTest.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
     <p>nuxt-img below here</p>
-    <nuxt-img
-      class="object-cover w-full md:rounded-l-xl"
-      style="height: 328px"
-      src="/treehouse_far_view.jpg"
-    />
+    <nuxt-img src="/treehouse_far_view.jpg" />
     <div>
       reg img element here
       <img src="/treehouse_far_view.jpg" alt="" />
