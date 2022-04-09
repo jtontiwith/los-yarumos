e51e1b2769321bb08047d9d44f280e9c0c6a60f7
jtontiwith <jtonti@gmail.com>
2022-04-09 14:47:44 -0500

    more testing

--- components/ImgTest.vue
+++ components/ImgTest.vue
@@ -10,6 +10,8 @@
       reg img element here
       <img src="/treehouse_far_view.jpg" alt="" />
     </div>
+    logo below
+    <nuxt-img class="hidden w-auto h-16 rounded-lg lg:block" src="/logo.jpg" />
   </div>
 </template>
 
