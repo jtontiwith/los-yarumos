52de5c8c32ec0cc2c3325bc5b179b455abb8bb07
jtontiwith <jtonti@gmail.com>
2022-04-09 14:37:54 -0500

    test to figure out img error in production

---
+++ components/ImgTest.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>nuxt-img below here</p>
+    <nuxt-img
+      class="object-cover w-full md:rounded-l-xl"
+      style="height: 328px"
+      src="/treehouse_far_view.jpg"
+    />
+    <div>
+      reg img element here
+      <img src="/treehouse_far_view.jpg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
