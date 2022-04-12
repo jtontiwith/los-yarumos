8ca387a30a25ee80efe26fa463cca9b3d7eb2eab
jtontiwith <jtonti@gmail.com>
2022-04-12 15:50:14 -0500

    make quick loading skeleton for image gallery

--- components/AppGallery.vue
+++ components/AppGallery.vue
@@ -1,50 +1,58 @@
 <template>
-  <section class="grid lg:grid-cols-2">
-    <div>
-      <nuxt-img
-        class="object-cover w-full h-full rounded lg:mb-0 lg:rounded-l-xl lg:rounded-r-none"
-        src="/treehouse_far_view_p.jpg"
-      />
-    </div>
-    <div class="flex flex-wrap pt-2 lg:pt-0 lg:pl-2">
-      <div
-        v-for="(img, i) in images"
-        :key="i"
-        class="w-1/2"
-        :class="{ 'pr-2 pb-2': i === 0, 'pb-2': i === 1, 'pr-2': i === 2 }"
-      >
+  <div>
+    <section v-show="isLoaded" class="grid lg:grid-cols-2">
+      <div>
         <nuxt-img
-          class="object-cover w-full h-full rounded"
-          :class="{
-            'lg:rounded-none': i === 0 || i === 2,
-            'lg:rounded-tr-xl lg:rounded-l-none lg:rounded-br-none': i === 1,
-            'lg:rounded-br-xl lg:rounded-l-none lg:rounded-tr-none': i === 3,
-          }"
-          :src="img.path"
+          class="object-cover w-full h-full rounded lg:mb-0 lg:rounded-l-xl lg:rounded-r-none"
+          :src="images[0].path"
+          @load="registerLoad(0)"
         />
       </div>
-    </div>
-  </section>
+      <div class="flex flex-wrap pt-2 lg:pt-0 lg:pl-2">
+        <div
+          v-for="(img, i) in images.slice(1)"
+          :key="i"
+          class="w-1/2"
+          :class="{ 'pr-2 pb-2': i === 0, 'pb-2': i === 1, 'pr-2': i === 2 }"
+        >
+          <nuxt-img
+            class="object-cover w-full h-full rounded"
+            :class="{
+              'lg:rounded-none': i === 0 || i === 2,
+              'lg:rounded-tr-xl lg:rounded-l-none lg:rounded-br-none': i === 1,
+              'lg:rounded-br-xl lg:rounded-l-none lg:rounded-tr-none': i === 3,
+            }"
+            :src="img.path"
+            @load="registerLoad(i + 1)"
+          />
+        </div>
+      </div>
+    </section>
+    <AppGallerySkeleton v-show="!isLoaded" />
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
     images: [
-      { path: '/bedroom_p.jpg', loaded: true },
+      { path: '/treehouse_far_view_p.jpg', loaded: false },
+      { path: '/bedroom_p.jpg', loaded: false },
       { path: '/bathroom_p.jpg', loaded: false },
       { path: '/kitchen_p.jpg', loaded: false },
       { path: '/balcony_p.jpg', loaded: false },
     ],
-    imgIsLoaded: false,
   }),
-  /* methods: {
+  computed: {
     isLoaded() {
-      this.imgIsLoaded = true
+      const loaded = this.images.map((i) => i.loaded)
+      return loaded.every(Boolean)
     },
-    smallLoaded(idx) {
+  },
+  methods: {
+    registerLoad(idx) {
       this.images[idx].loaded = true
     },
-  }, */
+  },
 }
 </script>
---
+++ components/AppGallerySkeleton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid w-full text-center h-96 lg:grid-cols-2 animate-pulse">
+    <div
+      class="w-full h-full bg-gray-200 rounded lg:mb-0 lg:rounded-l-xl lg:rounded-r-none"
+    ></div>
+    <div>
+      <div class="flex flex-wrap w-full h-full pt-2 lg:pt-0 lg:pl-2">
+        <div
+          v-for="(skel, i) in 4"
+          :key="i"
+          class="w-1/2"
+          :class="{ 'pr-2 pb-2': i === 0, 'pb-2': i === 1, 'pr-2': i === 2 }"
+        >
+          <div
+            class="w-full h-full bg-gray-200"
+            :class="{
+              'lg:rounded-none': i === 0 || i === 2,
+              'lg:rounded-tr-xl lg:rounded-l-none lg:rounded-br-none': i === 1,
+              'lg:rounded-br-xl lg:rounded-l-none lg:rounded-tr-none': i === 3,
+            }"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
